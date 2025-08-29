2230252779ac444234dc5dbf5f8fab691f9e4feb
Au Dang <au.dang.jx@renesas.com>
2025-08-29 13:17:13 +0700

    rz-disto: audio-init: Update audio command for RZ/G2L seies, V2L, G3S and V2H
    Update audio settings initscript for all RZ supported platforms.
    
    Signed-off-by: Au Dang <au.dang.jx@renesas.com>

--- meta-rz-distro/recipes-extend/audio-init/audio-init/audio.sh
+++ meta-rz-distro/recipes-extend/audio-init/audio-init/audio.sh
@@ -2,38 +2,58 @@
 
 hostname=$(cat /etc/hostname 2>/dev/null)
 
-case "$hostname" in
-  smarc-rzg2ul | smarc-rzg2l | smarc-rzg2lc)
-    amixer cset name='Left Input Mixer L2 Switch' on
-    amixer cset name='Right Input Mixer R2 Switch' on
-    amixer cset name='Headphone Playback Volume' 100
-    amixer cset name='PCM Volume' 100%
-    amixer cset name='Input PGA Volume' 25
-    ;;
-  smarc-rzg3e)
+set_common_g3e_g3s_v2h() {
+    # SSI-DA7212
+    # These commands are required when Playback/Capture
+
     amixer cset name='Aux Switch' on
     amixer cset name='Mixin Left Aux Left Switch' on
     amixer cset name='Mixin Right Aux Right Switch' on
     amixer cset name='ADC Switch' on
     amixer cset name='Mixout Right Mixin Right Switch' off
     amixer cset name='Mixout Left Mixin Left Switch' off
-    amixer cset name='Headphone Volume' 100%
+    amixer cset name='Headphone Volume' 70%
     amixer cset name='Headphone Switch' on
     amixer cset name='Mixout Left DAC Left Switch' on
     amixer cset name='Mixout Right DAC Right Switch' on
     amixer cset name='DAC Left Source MUX' 'DAI Input Left'
     amixer cset name='DAC Right Source MUX' 'DAI Input Right'
+
     amixer sset 'Mic 1 Amp Source MUX' 'MIC_P'
     amixer sset 'Mic 2 Amp Source MUX' 'MIC_P'
     amixer sset 'Mixin Left Mic 1' on
-    amixer sset 'Mixin Left Mic 1' on
-    amixer sset 'Mic 1' 100% on
-    amixer sset 'Mic 2' 100% on
-    amixer sset 'Lineout' 100% on
+    amixer sset 'Mixin Right Mic 2' on
+    amixer sset 'Mic 1' 90% on
+    amixer sset 'Mic 2' 90% on
+    amixer sset 'Lineout' 80% on
     amixer sset 'Headphone' 100% on
     amixer sset 'Mixin PGA' 40% on
+}
+
+case "$hostname" in
+  smarc-rzg2ul | smarc-rzg2l | smarc-rzg2lc | smarc-rzv2l)
+    amixer cset name='Left Input Mixer L2 Switch' on
+    amixer cset name='Right Input Mixer R2 Switch' on
+    amixer cset name='Headphone Playback Volume' 100
+    amixer cset name='PCM Volume' 100%
+    amixer cset name='Input PGA Volume' 25
+    ;;
+
+  smarc-rzg3s)
+    set_common_g3e_g3s_v2h
+    ;;
+
+  smarc-rzg3e)
+    set_common_g3e_g3s_v2h
     amixer sset 'DVC In',0 10%
     ;;
+
+  rzv2h-evk)
+    set_common_g3e_g3s_v2h
+    amixer sset 'DVC In',0 10%
+    amixer sset 'DVC Out',0 20%
+    ;;
+
   *)
     ;;
 esac
