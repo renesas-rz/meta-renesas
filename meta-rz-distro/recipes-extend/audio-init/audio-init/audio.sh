renesas-rz/meta-renesas

commit dd938e8637d5f1ac3b3ed4334ee91ad8875b5d7f

audio-init: Init mixer only when an ALSA card is present
Guard the audio initialization script to run only when at least one ALSA
sound card is available. If no card is detected, skip mixer setup to avoid
amixer errors during login/boot on systems where the audio driver is not
probed.

Also add DVC output support for RZ/G3E.

Signed-off-by: Tranh Ha <tranh.ha.xb@renesas.com>

meta-rz-distro/recipes-extend/audio-init/audio-init/audio.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if ! grep -qE '^[[:space:]]*[0-9]+' /proc/asound/cards 2>/dev/null; then
+	echo "No soundcard detected."
+	return 0
+fi
+
 hostname=$(cat /etc/hostname 2>/dev/null)
 
 set_common_g3e_g3s_v2h() {
@@ -42,12 +47,7 @@ case "$hostname" in
     set_common_g3e_g3s_v2h
     ;;
 
-  smarc-rzg3e)
-    set_common_g3e_g3s_v2h
-    amixer sset 'DVC In',0 10%
-    ;;
-
-  rzv2h-evk)
+  smarc-rzg3e | rzv2h-evk)
     set_common_g3e_g3s_v2h
     amixer sset 'DVC In',0 10%
     amixer sset 'DVC Out',0 20%

commit 120ca3e740a8ee2e052c06b3a9f67f797fff97e6

rz-distro: audio-init: Skip mixer initialization when DA7213 is not enabled
Add a check for the DA7213 codec's presence before running ALSA amixer
commands on the RZ/{G3S,G3E,G3L} Smarc EVK boards
and RZ/{V2H,V2N} EVK boards.

This prevents mixer initialization from being applied when the DA7213
codec is not properly enabled.

Fixes: dd938e8637 ("audio-init: Init mixer only when an ALSA card is present")
Signed-off-by: Nghia Nguyen <nghia.nguyen.xm@renesas.com>

meta-rz-distro/recipes-extend/audio-init/audio-init/audio.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 if ! grep -qE '^[[:space:]]*[0-9]+' /proc/asound/cards 2>/dev/null; then
-	echo "No soundcard detected."
-	return 0
+  echo "No soundcard detected."
+  return 0
 fi
 
 hostname=$(cat /etc/hostname 2>/dev/null)
+codec_name="da7213"
 
 set_common_g3e_g3s_v2h_v2n() {
+  if aplay -l 2>/dev/null | grep -i "$codec_name" >/dev/null || \
+     arecord -l 2>/dev/null | grep -i "$codec_name" >/dev/null; then
     # SSI-DA7212
     # These commands are required when Playback/Capture
-
     amixer cset name='Aux Switch' on
     amixer cset name='Mixin Left Aux Left Switch' on
     amixer cset name='Mixin Right Aux Right Switch' on
@@ -32,6 +34,11 @@ set_common_g3e_g3s_v2h_v2n() {
     amixer sset 'Mic 2' 80% on
     amixer sset 'Lineout' 80% on
     amixer sset 'Mixin PGA' 40% on
+    return 0
+  else
+    echo "Codec $codec_name not found. Skip amixer settings."
+    return 1
+  fi
 }
 
 case "$hostname" in